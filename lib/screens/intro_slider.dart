201ae0eb2defc127840845265ffa2fbb8185bf3d
HeveshL <hevesh000@gmail.com>
2020-03-02 15:06:40 +0530

    created profile screen

--- lib/screens/intro_slider.dart
+++ lib/screens/intro_slider.dart
@@ -2,8 +2,6 @@ import 'package:intro_slider/intro_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:intro_slider/slide_object.dart';
 
-
-
 class IntoScreen extends StatefulWidget {
   @override
   _IntoScreenState createState() => _IntoScreenState();
@@ -18,26 +16,28 @@ class _IntoScreenState extends State<IntoScreen> {
 
     slides.add(
       new Slide(
-        title: "ERASER",
-        description: "Allow miles wound place the leave had. To sitting subject no improve studied limited",
-        backgroundColor: Colors.white,
-      ),
+          title: "ERASER",
+          description:
+              "Allow miles wound place the leave had. To sitting subject no improve studied limited",
+          styleDescription: TextStyle(color: Colors.black),
+          styleTitle: TextStyle(color: Colors.black)),
     );
     slides.add(
       new Slide(
-        title: "PENCIL",
-        description: "Ye indulgence unreserved connection alteration appearance",
-        backgroundColor: Colors.white,
-      ),
+          title: "PENCIL",
+          description:
+              "Ye indulgence unreserved connection alteration appearance",
+          styleDescription: TextStyle(color: Colors.black),
+          styleTitle: TextStyle(color: Colors.black)),
     );
     slides.add(
       new Slide(
-        title: "RULER",
-        description:
-        "Much evil soon high in hope do view. Out may few northward believing attempted. Yet timed being songs marry one defer men our. Although finished blessing do of",
-        backgroundColor: Colors.white,
-        
-      ),
+          title: "RULER",
+          description:
+              "Much evil soon high in hope do view. Out may few northward believing attempted. Yet timed being songs marry one defer men our. Although finished blessing do of",
+          styleTitle: TextStyle(color: Colors.black),
+          styleDescription: TextStyle(color: Colors.black),
+          backgroundColor: Colors.transparent),
     );
   }
 
@@ -47,9 +47,25 @@ class _IntoScreenState extends State<IntoScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return new IntroSlider(
-      slides: this.slides,
-      onDonePress: this.onDonePress,
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        colors: [
+          Color.fromRGBO(127, 0, 255, 1),
+          Colors.indigo,
+          Colors.blue,
+        ],
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+      )),
+      child: new IntroSlider(
+        slides: this.slides,
+        onDonePress: this.onDonePress,
+        //backgroundColorAllSlides: Colors.transparent,
+        colorDoneBtn: Colors.black,
+        colorPrevBtn: Colors.black,
+        colorSkipBtn: Colors.black,
+      ),
     );
   }
 }
