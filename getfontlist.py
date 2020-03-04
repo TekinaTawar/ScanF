8e07c5c94a77366f7b4ec503977214cbbb00401b
Shubham <joshisd@rknec.edu>
2020-03-04 18:32:24 +0530

    added fonts in yaml

--- getfontlist.py
+++ getfontlist.py
@@ -4,14 +4,16 @@ text = ''
 
 fontfamilies = os.listdir(r'./assets/fonts')
 for fontfamily in fontfamilies:
-    text += '- family: '+ fontfamily + '\n'
+    text += '  - family: '+ fontfamily + '\n'
     fontaddress = r'./assets/fonts/'+fontfamily
     fonts = os.listdir(fontaddress)
-    text += '\tfonts:\n'
+    text += '      fonts:\n'
     for font in fonts:
-        text += '- asset: ' + fontaddress + font + '\n'
+        text += '        - asset: ' + fontaddress+'/' + font + '\n'
 
 
+with open('./abc.txt', 'w') as file:
+    file.write(text)
     
 
 
