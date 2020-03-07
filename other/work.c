ff83305fc8ac2677e03feaf61bfdf7c5c6a38b1d
TekinaTawar <astroaniket1@gmail.com>
2020-03-07 10:49:16 +0530

    added some other files

---
+++ other/work.c
@@ -0,0 +1,19 @@
+
+
+
+
+// ? How to use gitignore file to not track chages in some of the file in vs code. 
+
+// * Important information
+//! To delete
+//todo: 
+
+
+
+
+
+
+
+
+
+// use the extension better comments to use this file properly.
