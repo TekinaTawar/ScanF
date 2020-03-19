745595ee5e8e47848cc718f6f67796584b765fee
HeveshL <hevesh000@gmail.com>
2020-03-19 20:32:53 +0530

    updated todos

--- other/work.c
+++ other/work.c
@@ -1,12 +1,24 @@
 // ? How to use gitignore file to not track chages in some of the file in vs code. 
 
-//todo: Manage State of dropdown
-//todo: Black screen issue
+//todo: Manage State of dropdown(find effective way to use dropdowns)
+//todo: Black screen issue on poping and sudden pushing screens
 //todo: Loading time of svg
-//todo: Transitions between pages
+//todo: Add + button functionality in create profile screen 3
+//todo: Make create profile screens scrollable to prevent overflowing pixels (test.dart)
+//todo: Make next button in create profile screens to look better
+//todo: Make text boxes more easy to next rather to only presss tick button on keyboard
+//todo: Change focus color of text boxes as of non-focused box
+
+//todo: Check test file for create profile screen
+//todo: Change fonts in test file for create profile screen
+//todo: If no issues in test make it as final create profile screen
+
+//todo: To create screen 4 for create profile
 
 
-// * Done:
+
+// * Done: Add Transitions between pages
+
 //! To delete
 
 // use the extension better comments to use this file properly.
